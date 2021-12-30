b197807e264161fd18211942224d9fe71207d768
Quentin V <quentin.vauthier000@gmail.com>
2021-12-30 03:09:34 +0100

    Changed handling, no more intervals, events are much better

--- content-script.js
+++ content-script.js
@@ -1,43 +1,40 @@
-var previousUrl = null
-var intervals = []
 var username = null
-var previousAsLength = 0
 
-detectUrlChange()
-setInterval(detectUrlChange, 1000)
+const notReleasedVolume = '<div class="css-1dbjc4n"><svg viewBox="0 0 512 512" width="30" height="30" fill="var(--colors-icon)" stroke="var(--colors-icon)"><rect width="416" height="352" rx="48" ry="48" fill="none" stroke-width="32" stroke-linejoin="round" x="48" y="80"></rect><circle cx="336" cy="176" r="32" fill="none" stroke-width="32" stroke-miterlimit="10"></circle><path d="M304 335.79l-90.66-90.49a32 32 0 00-43.87-1.3L48 352M224 432l123.34-123.34a32 32 0 0143.11-2L464 368" fill="none" stroke-width="32" stroke-linecap="round" stroke-linejoin="round"></path></svg></div>'
+const observer = new MutationObserver((mutationList, observer) => {
+	// Guard case to prevent triggering for nothing, will only trigger if all added elements are Images, so new volumes OR if there is a not
+	if(!mutationList.every(m => m?.addedNodes?.[0]?.constructor?.name === 'HTMLImageElement') && !mutationList.some(m => m?.addedNodes?.[0]?.innerHTML == notReleasedVolume)) return
+	let url = window.location.href
+	handleEditions(url.substr(url.indexOf('/editions/')+'/editions/'.length))
+})
+const observerConfig = { childList: true, subtree: true };
 
-function detectUrlChange() {
-	let url = globalThis.location.href
-	if(url != previousUrl) {
-		previousUrl = url
-		handleURLChange(url)
-	}
-}
+// Detect url change and triggers handleURLChange
+window.onpopstate = () => handleURLChange(window.location.href)
+window.onpopstate() // Exec the function at the beginning
 
 function handleURLChange(url) {
-	while(intervals.length > 0) clearInterval(intervals.shift())
+	observer.disconnect() // Disconnect the observer when changing url e.g. going from edition to volume
 	// Refresh the username
 	chrome.storage.sync.get(['cave-collec-username'], obj => {
 		username = obj['cave-collec-username']
 		if(!username) return
-		if(url.includes('/editions/')) {
-			previousAsLength = 0
+		if(url.includes('/editions/')) { // If in edition page
 			handleEditions(url.substr(url.indexOf('/editions/')+'/editions/'.length))
-			intervals.push(setInterval(() => handleEditions(url.substr(url.indexOf('/editions/')+'/editions/'.length)), 1000))
-		}else if(url.includes('/volumes/')) {
+			observer.observe(document.querySelector('div[class="css-1dbjc4n r-eqz5dr"]').firstChild, observerConfig)
+		}else if(url.includes('/volumes/')) { // Volume page
 			handleVolume(url.substr(url.indexOf('/volumes/')+'/volumes/'.length))
 		}
 	})
 }
 
-
 function handleEditions(editionId) {
 	let as = document.querySelectorAll('a[href]')
-	if(previousAsLength != as.length) previousAsLength = as.length
-	else return;
+	let volumes = []
+	for(const a of as) if(a.href.match('.*/volumes/.*')) volumes.push(a)
 	let idToEl = new Map()
-	as.forEach(a => {
-	    if(a.href.includes('volumes')) idToEl.set(a.href.substr(a.href.indexOf('/volumes/')+'/volumes/'.length), a)
+	volumes.forEach(a => {
+	    idToEl.set(a.href.substr(a.href.indexOf('/volumes/')+'/volumes/'.length), a)
 	})
 	let ids = Array.from(idToEl.keys())
 	let url = `https://quinta.ovh:3443/api/edition/${editionId}/${username}`
@@ -46,17 +43,15 @@ function handleEditions(editionId) {
 		jsonResp.forEach(r => {
 			if(!r.read) return
 			let el = idToEl.get(r.mangaId)
-			if(el.dataset.read) return
-			let checkRed =  el.children[0].children[1].children[0].cloneNode(true)
-			if(!checkRed) return
-			checkRed.style.backgroundColor = '#DDDD55'
-			el.children[0].children[1].appendChild(checkRed)
+			if(!el || el.dataset.read) return
+			let readCheck =  createReadCheck()
+			el.children[0].children[1].appendChild(readCheck)
 			el.dataset.read = true
 		})
 	})
 }
 
-function handleVolume(volumeId) {
+function handleVolume(volumeId) { // Creates the read / not read button and put it on the page
 	let collec = document.querySelectorAll('div[class="css-1dbjc4n r-18u37iz"]')[2]
 	let banner = collec.parentElement.parentElement
 
@@ -92,7 +87,7 @@ function handleVolume(volumeId) {
 	})
 }
 
-function notRead2Read(readDiv, a, volumeId, execRequest = true) {
+function notRead2Read(readDiv, a, volumeId, execRequest = true) { // Changes not read to read and sends the request to the server if needed
 	a.innerHTML = 'LU'
 	readDiv.style.backgroundColor = '#4c76ad'
 	readDiv.onclick = () => read2NotRead(readDiv, a, volumeId)
@@ -100,7 +95,7 @@ function notRead2Read(readDiv, a, volumeId, execRequest = true) {
 	request('PUT', `https://quinta.ovh:3443/api/read/${volumeId}/${username}`, {read: true}, null)
 }
 
-function read2NotRead(readDiv, a, volumeId, execRequest = true) {
+function read2NotRead(readDiv, a, volumeId, execRequest = true) { // Changes read to not read and sends the request to the server if needed
 	a.innerHTML = 'PAS LU'
 	readDiv.style.backgroundColor = '#cf783a'
 	readDiv.onclick = () => notRead2Read(readDiv, a, volumeId)
@@ -108,11 +103,19 @@ function read2NotRead(readDiv, a, volumeId, execRequest = true) {
 	request('PUT', `https://quinta.ovh:3443/api/read/${volumeId}/${username}`, {read: false}, null)
 }
 
-function request(method, url, body, callback) {
+function request(method, url, body, callback) { // Sends a request
 	var oReq = new XMLHttpRequest();
 	oReq.open(method, url, true);
 	if(typeof callback === 'function') oReq.onload = (res) => callback(oReq)
 	oReq.setRequestHeader("Content-Type", "application/json; charset=utf-8")
 	jsonBody = JSON.stringify(body);
 	oReq.send(jsonBody);
+}
+
+function createReadCheck() {
+	const readCheckHtml = '<div data-readcheck="true" class="css-1dbjc4n r-1awozwy r-y47klf r-1yadl64 r-18u37iz r-1wbh5a2 r-157gdtw r-1777fci r-ilng1c r-m2pi6t r-1hvjb8t r-cacpof" style="background-color: rgb(221, 221, 85);"><div class="css-1dbjc4n r-1mdbw0j"><svg viewBox="0 0 512 512" width="16" height="16" fill="#FFF" stroke="#FFF"><path d="M416 128L192 384l-96-96" fill="none" stroke-width="44" stroke-linecap="round" stroke-linejoin="round"></path></svg></div></div>'
+	var div = document.createElement('div');
+	div.innerHTML = readCheckHtml;
+	// Change this to div.childNodes to support multiple top-level nodes
+	return div.firstChild;
 }
